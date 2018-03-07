cb95b91c41b475c34712aa9f40ed1ea578801262
Aaron Weiss <awe@pdgn.co>
2018-03-08 00:30:13 +0100

    Added a bunch of progress on a basic UI.

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
+  <main id="app">
     <router-view/>
-  </div>
+  </main>
 </template>
 
 <script>
@@ -12,12 +11,14 @@ export default {
 </script>
 
 <style>
+html, body, #app {
+    height: 100%;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,113 +1,191 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
-  </div>
+  <section id="root">
+    <section id="server-list">
+      <ul>
+        <li class="server">
+          FyreChat
+          <ul>
+            <li class="channel">#vana</li>
+            <li class="channel">#vana-commits</li>
+          </ul>
+        </li>
+        <li>
+          PdgnCo
+          <ul>
+            <li class="channel">#pdgn</li>
+            <li class="channel">#merveille</li>
+          </ul>
+        </li>
+      </ul>
+    </section>
+    <section id="buffer-view">
+      <section id="chat-buffer">
+        <section id="topic">
+          PdgnCo: https://pdgn.co/ | Canary: https://pdgn.co/canary.txt | Stats: http://ircstats.wogloms.com/pisg/pdgn
+        </section>
+        <section id="messages">
+          <div class="message" v-for="msg in messages" :key="msg.id">
+            <span class="message-time">{{ msg.time }}</span>
+            <span class="message-author">{{ msg.author }}</span>
+            <span class="message-body">{{ msg.content }}</span>
+          </div>
+        </section>
+      </section>
+      <section id="chan-list">
+        <ul>
+          <li class="user">
+            <div class="role role-voice"></div>
+            <div class="nickname">awe</div>
+          </li>
+          <li class="user">
+            <div class="role role-voice"></div>
+            <div class="nickname">awebot</div>
+          </li>
+          <li class="user">
+            <div class="role"></div>
+            <div class="nickname">big_meech</div>
+          </li>
+        </ul>
+      </section>
+    </section>
+    <section id="chat-input">
+      <input placeholder="Send message...">
+    </section>
+  </section>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  data () {
+  data: function () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      messages: Array(100).fill().map((_, i) => {
+        return { id: i, time: '20:08', author: 'awe', content: 'ay le mayo' }
+      })
     }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
+<style>
+#root {
+  height: 100%;
+  display: grid;
+  grid-template-columns: 1fr 6fr;
+  grid-template-rows: 15fr 1fr;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+#server-list {
+  border-right: 1px solid black;
 }
-li {
+
+#server-list ul {
+  list-style: none;
+  margin: 0;
+  padding: 1em;
+}
+
+#server-list ul li.server {
+  margin-bottom: 1em;
+}
+
+#server-list ul ul {
+  padding: 0.3em 1em;
+}
+
+#buffer-view {
+  display: grid;
+  grid-template-columns: 6fr 1fr;
+}
+
+#server-list, #chat-buffer #messages, #chan-list {
+  padding: 0.5em;
+}
+
+#chan-list {
+  border-left: 1px solid black;
+}
+
+#chan-list ul {
+  list-style: none;
+  margin: 0;
+  padding: 1em;
+}
+
+#chan-list li {
+  margin: 0.1em 0;
+}
+
+#chan-list .role, #chan-list .nickname {
   display: inline-block;
-  margin: 0 10px;
+  vertical-align: middle;
 }
-a {
-  color: #42b983;
+
+#chan-list .role {
+  display: inline-grid;
+  justify-content: center;
+  align-content: center;
+  border-radius: 0.3em;
+  width: 1.2em;
+  height: 1.2em;
+  background: #eee;
+  color: white;
+  font-weight: bold;
+}
+
+#chan-list .role-voice {
+  background: #7eadff;
+}
+
+#chan-list .role-voice::after {
+  content: '+';
+}
+
+#chat-buffer {
+  display: grid;
+  grid-template-rows: 2.2em auto;
+}
+
+#chat-buffer #topic {
+  align-self: top;
+  padding: 0.5em;
+  border-bottom: 1px solid black;
+  overflow: hidden;
+}
+
+#chat-buffer #messages {
+  align-self: end;
+  max-height: calc(100% - 1em);
+  overflow: scroll;
+}
+
+.message-time::before {
+  content: "[";
+}
+
+.message-time::after {
+  content: "]";
+}
+
+.message-author::before {
+  content: "(";
+}
+
+.message-author::after {
+  content: ")";
+}
+
+#chat-input {
+  grid-column: span 2;
+  padding: 1em;
+  border-top: 1px solid black;
+}
+
+#chat-input input {
+  width: calc(100% - 2em);
+  border: 0;
+  background: #eee;
+  padding: 0.5em 1em;
+  border-radius: 0.3em;
 }
 </style>
