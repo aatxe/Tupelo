ef274902a4a1a21fc3609295abf08c45ad81a923
Aaron Weiss <awe@pdgn.co>
2018-03-21 00:27:08 +0100

    Added a 404 page for unknown routes.

---
+++ src/components/NotFound.vue
@@ -0,0 +1,33 @@
+<template>
+<section id="splash">
+  <h1>404</h1>
+  <div class="message"><code>{{ path }}</code> is not a valid route.</div>
+</section>
+</template>
+
+<script>
+export default {
+  name: 'NotFound',
+
+  computed: {
+    path: function () {
+      return this.$route.fullPath
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+@require '../vars'
+
+#splash
+  background: buffer-bg-color
+  align-items: center
+  justify-content: center
+  height: 100vh
+  display: flex
+  flex-direction: column
+
+  h1
+    margin: 0.1em 0
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import NotFound from '@/components/NotFound'
+
 import ChannelView from '@/components/ChannelView'
 import QueryView from '@/components/QueryView'
 import ServerView from '@/components/ServerView'
@@ -12,6 +14,7 @@ export default new Router({
     { path: '/', redirect: '/chan/#pdgn' },
     { path: '/chan/:chan', component: ChannelView, props: { showUserList: true } },
     { path: '/server/:server', component: ServerView },
-    { path: '/query/:user', component: QueryView }
+    { path: '/query/:user', component: QueryView },
+    { path: '*', component: NotFound }
   ]
 })
